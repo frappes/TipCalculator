113ca71444890cf5330db15d6de1e1cac65eda0b
Ross Danielson <rdanielson@zynga.com>
2014-01-14 20:12:32 -0800

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Ross Danielson on 1/14/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tip Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tip Calculator
+//
+//  Created by Ross Danielson on 1/14/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
